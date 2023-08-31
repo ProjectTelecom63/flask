586d0de9e9f2e212a414d9fccefe7bcd5ac9d954
ProjectTelecom63 <141921426+ProjectTelecom63@users.noreply.github.com>
2023-08-31 10:06:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -238,4 +238,4 @@ def delete_values():
     return "All values deleted successfully!"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host="0.0.0.0")
