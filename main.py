f83f52abd897825171a425bf8e9c790c7e58662c
ProjectTelecom63 <141921426+ProjectTelecom63@users.noreply.github.com>
2023-12-29 09:31:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 
 url = "http://rung.ddns.net:8050"
@@ -34,6 +34,37 @@ def api_node():
         data = response.json()
 
         return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        print(f"Error making the request: {e}")
+        return jsonify({"error": "Failed to get API response"}), 500
+
+
+@app.route("/api/show/today", methods=["GET"])
+def api_today():
+    urlnode = url + "/api/show/today"
+
+    try:
+        response = requests.get(urlnode)
+        response.raise_for_status()  # Check for errors in the response
+
+        # Assuming the response is in JSON format
+        data = response.json()
+
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        print(f"Error making the request: {e}")
+        return jsonify({"error": "Failed to get API response"}), 500
+
+
+@app.route("/api/activate", methods=["GET"])
+def activate():
+    try:
+        nodename = request.args.get("nodename")
+        urlact = url + "/api/activate?nodename=" + nodename
+        response = requests.get(urlact)
+        response.raise_for_status()
+
+        return response.text
     except requests.exceptions.RequestException as e:
         print(f"Error making the request: {e}")
         return jsonify({"error": "Failed to get API response"}), 500
