b663a0faf8c7306cfc12b04c11864caec5aeef84
ProjectTelecom63 <141921426+ProjectTelecom63@users.noreply.github.com>
2024-02-12 15:39:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 
 url = "http://rung.ddns.net:8050"
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Enable CORS for the entire app
 
 
 @app.route("/api/show", methods=["GET"])
@@ -87,6 +87,84 @@ def fetch_data():
         response = requests.post(urldate, json=payload, headers=headers)
 
         data = response.json()
+        return jsonify(data)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error making the request: {e}")
+        return jsonify({"error": "Failed to get API response"}), 500
+
+
+@app.route("/api/alert/changecon", methods=["POST"])
+def change_con():
+    try:
+        data = request.json
+        print(data)
+        urlcon = url + "/api/alert/changecon"
+        print(urlcon)
+        temperature = data.get("Temperature")
+        humidity = data.get("Humidity")
+        speed = data.get("Speed")
+        longitude = data.get("Longitude")
+        latitude = data.get("Latitude")
+        radius = data.get("Radius")
+        nbat = data.get("NBattery")
+        gbat = data.get("GBattery")
+        payload = {
+            "Temperature": temperature,
+            "Humidity": humidity,
+            "Speed": speed,
+            "Longitude": longitude,
+            "Latitude": latitude,
+            "Radius": radius,
+            "NBattery": nbat,
+            "GBattery": gbat,
+        }
+        print(payload)
+        headers = {"Content-Type": "application/json"}
+
+        requests.post(urlcon, json=payload, headers=headers)
+
+        return jsonify(data)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error making the request: {e}")
+        return jsonify({"error": "Failed to get API response"}), 500
+
+
+@app.route("/api/alert/con", methods=["GET"])
+def con():
+    try:
+        urlcon = url + "/api/alert/con"
+        response = requests.get(urlcon)
+        response.raise_for_status()  # Check for errors in the response
+
+        # Assuming the response is in JSON format
+        data = response.json()
+
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        print(f"Error making the request: {e}")
+        return jsonify({"error": "Failed to get API response"}), 500
+
+
+@app.route("/api/addemail", methods=["POST"])
+def insert_email():
+    try:
+        data = request.json
+        print(data)
+        urlcon = url + "/api/alert/addemail"
+        print(urlcon)
+        email = request.json.get("email", None)
+        delay = request.json.get("delay", None)
+        payload = {
+            "email": email,
+            "delay": delay,
+        }
+        print(payload)
+        headers = {"Content-Type": "application/json"}
+
+        requests.post(urlcon, json=payload, headers=headers)
+
         return jsonify(data)
 
     except requests.exceptions.RequestException as e:
